df2254bc9ec7340efc250804d5a3c059342cecf2
Alavalapalli Vasanth Reddy <alavalapalligithub@gmail.com>
2023-08-15 23:30:19 +0530

    added a pythonfile

---
+++ python.py
@@ -0,0 +1,24 @@
+decimal=10 #decimal to binary
+bin_number=bin(decimal)
+print(bin_number)
+
+binary_number='1010' #binary to decimal
+bin_num=int(binary_number,2)
+print(bin_num)
+
+decimal_number=10 #decimal to octal
+octal_number=oct(decimal_number)
+print(octal_number)
+
+octal_num='12' #octal to decimal
+decimal_num=int(octal_num,8)
+print(decimal_num)
+
+decimal_num=10 #decimal to hexa
+hex_num=hex(decimal_num)
+print(hex_num)
+
+hex_num='a' # hexa to decimal
+decimal_num=int(hex_num,16)
+print(decimal_num)
+
